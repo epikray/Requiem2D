37f00739def90c098119bc3639674d7dd071028b
Hljung <henlj389@student.liu.se>
2026-05-03 13:57:45 +0200

    organization

--- Scenes/Levels/test_level.gd
+++ Scenes/Levels/test_level.gd
@@ -1,21 +1,11 @@
 extends Node2D
 
-@export var sprite : Sprite2D
-@export var spriteAnimPlayer: AnimationPlayer
-
-enum ANIM_STATE {I_F, I_L, I_B, I_R, W_F, W_L, W_B, W_R}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func _process_state(delta: float) -> void: 
-	pass
-	
